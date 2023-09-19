171df8fc36fcef60616cfe75968d9161f80a22e1
kiranromi <kiran.romi@gmail.com>
2023-09-19 16:07:48 -0400

    commented our code on sep 19th

---
+++ secondproject/src/newpackagevariables/EscapeSequence.java
@@ -0,0 +1,31 @@
+package newpackagevariables;
+
+public class EscapeSequence {
+	public static void main(String[]args){
+		
+		// This is the example of a new line character \n
+		System.out.println("Good aftrnoon\n This is Kiran");
+		
+		System.out.println(" How are you\n I am fine");
+		
+		//This is an example of another way of new line character\n
+		
+		System.out.println("Good aftrnoon ");
+		System.out.println("This is Kiran ");
+		System.out.println("How are you"); 
+		System.out.println("I am fine");
+		
+		//Basic formating with tabs
+		System.out.println("1\tOne\tCo13");     
+		System.out.println("1\tOne\tCo13");
+		System.out.println("10\tTen\tCo13");
+		System.out.println("100\tOne Hundred\tCo13");
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ secondproject/src/operatorpackage/Variables.java
@@ -0,0 +1,42 @@
+package operatorpackage;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+			
+			 
+				
+				boolean conditionOne = true;
+				boolean conditionTwo = false;
+				
+				//&& is And
+			 if(conditionOne && conditionTwo){ 
+				//do something if one or the both variables are true
+				System.out.println("Both variables are true");
+			 }
+
+				
+			// || is OR	
+			if(conditionOne || conditionTwo){ 
+				//do something if one or the other or the both variables are true
+				System.out.println("Either or Both oof the variables is true");
+				
+				}
+			// neighter is true
+				if( !conditionOne && !conditionTwo){ 
+					//do something if one or the other or the both variables are true
+					System.out.println("Neighter is true");
+					}
+				
+				int x= 5;
+				if(x > 3 && x <10 )
+				{
+				System.out. println("x is between 3 and 10")	;
+					
+				}
+			
+
+	}
+
+	}
