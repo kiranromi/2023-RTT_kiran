985149fb254abef27a8c40da78e0e37e00b52254
kiranromi <kiran.romi@gmail.com>
2023-09-19 10:19:34 -0400

    variable example from class

---
+++ secondproject/src/newpackagevariables/AreaComputer.java
@@ -0,0 +1,25 @@
+package newpackagevariables;
+
+public class AreaComputer {
+
+	
+			  public static void main(String[] args) {
+			    double radius;
+			    double area;
+			    
+			    // Assign a radius
+			    radius = 20;
+			    
+			    // Compute area
+			    area = radius * radius * 3.14159;
+			    
+			    // Display results
+			    System.out.println("The area for the circle of radius " +
+			      radius + " is " + area);
+			  }
+			
+
+
+	}
+
+
